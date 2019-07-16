b04969e8bd3c42cede386b952825d79053c16d9b
Thitsugaya1 <32875817+Thitsugaya1@users.noreply.github.com>
2019-07-16 12:38:34 -0400

    Codigo enviado
    Coloque el codigo que me enviaste por wsp joaco

--- Directorio.java
+++ Directorio.java
@@ -10,5 +10,68 @@ package disco;
  * @author Joaquin
  */
 class Directorio {
+    private ArrayList<String> nombresArchivos;
+    private ArrayList<Integer> sectoresOcupados;
+    private int largoDirectorio;
+
+    public Directorio() {
+        this.nombresArchivos= new ArrayList<>();
+        this.sectoresOcupados= new ArrayList<>();
+        this.largoDirectorio= 0;
+    }
     
+    public void rellenadoArrayList() {
+        for(int i=0; i<largoDirectorio-1; i++) {
+            if(i==0) {
+                this.nombresArchivos.add("");
+                this.sectoresOcupados.add(1);
+            }
+            
+            else {
+                this.nombresArchivos.add("");
+                this.sectoresOcupados.add(0);
+            
+            }
+        }
+    }
+    
+    public void borrandoArray() {
+        for(int i=0; i<largoDirectorio-1; i++) {
+            if(i==0) {
+                this.nombresArchivos.add("");
+                this.sectoresOcupados.add(0);
+            }
+            
+            else {
+                this.nombresArchivos.add("");
+                this.sectoresOcupados.add(0);
+            
+            }
+        }
+    }
+    
+    
+    public String getNombresArchivos(int v) {
+        return this.nombresArchivos.get(v);
+    }
+
+    public void setNombresArchivos(int v, String nombre) {
+        this.nombresArchivos.set(v, nombre);
+    }
+
+    public int getSectoresOcupados(int v) {
+        return this.sectoresOcupados.get(v);
+    }
+
+    public void setSectoresOcupados(int v, int cambioEstado) {
+        this.sectoresOcupados.set(v, cambioEstado);
+    }
+
+    public int getLargoDirectorio() {
+        return this.largoDirectorio;
+    }
+
+    public void setLargoDirectorio(int largoDirectorio) {
+        this.largoDirectorio = largoDirectorio;
+    }
 }
